a16fb793a1c7a9416cfdb0b229543c9ff4f11a29
Aleksandra Lipert <ola.luszcz@interia.pl>
2019-02-20 21:45:21 +0100

    Remove untracekd files

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Title = props =>
-<div>
-<h1>{props.title}</h1>
-<p>Number of tasks: {props.numberOfTasks}</p>
-</div>;
+    <div>
+        <h1>{props.title}</h1>
+        <p>Number of tasks: {props.numberOfTasks}</p>
+    </div>;
 
 export default Title;
